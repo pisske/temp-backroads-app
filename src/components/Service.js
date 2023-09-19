939a9cdd92b2940ecfa5c0fd9a2ec45d99852476
Vladimir Pisacic <vladimirpisacic@gmail.com>
2023-09-19 00:19:23 -0400

    first commit

---
+++ src/components/Service.js
@@ -0,0 +1,27 @@
+import { services } from "../data";
+import Title from "./Title";
+
+const Service = () => {
+  return (
+    <section className='section services' id='services'>
+      <Title title='our' subTitle='services' />
+      <div className='section-center services-center'>
+        {services.map((service) => {
+          const { id, title, icon, text } = service;
+          return (
+            <article key={id} className='service'>
+              <span className='service-icon'>
+                <i className={icon}></i>
+              </span>
+              <div className='service-info'>
+                <h4 className='service-title'>{title}</h4>
+                <p className='service-text'>{text}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+export default Service;
